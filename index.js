923830bbf1c9766805ac07fe859fb529374d7229
Renzo Caccia <Renzo Caccia>
2017-05-13 15:08:41 -0300

    MySQL connection

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const http = require('http');
-
+const db = require('app/config/config.js');
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
@@ -12,4 +11,11 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+db.query('SELECT * from test1 ', function(err, rows, fields) {
+  if (!err)
+    console.log(rows);
+  else
+    console.log('Error while performing Query.');
 });
